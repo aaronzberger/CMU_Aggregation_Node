4f0038fa5c96492ffcbac8a43ebe7018f6a20d3d
Aaron Berger <aaronzberger@gmail.com>
2021-06-25 10:08:26 -0400

    Publish consecutive merged frames using queue

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CMU_Aggregation_Node
+
+ROS_NAMESPACE=/mapping rosrun stereo_image_proc stereo_image_proc _approximate_sync:=True _queue_size:=10
+ rosrun rqt_reconfigure rqt_reconfigure
+ rosrun image_view stereo_view stereo:=mapping image:=image_rect _approximate_sync:=True _queue_size:=10
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import rospy
 from rospy import topics
 from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import Header
+import time
 
 # Define how many frames to aggregate into one pointcloud
 AGGREGATION_COUNT = 10
@@ -28,29 +29,24 @@ class Aggregation_Node:
         self.pub_aggregated = rospy.Publisher(
             '/velodyne_aggregated', PointCloud2, queue_size=1)
 
-        self.frame_counter = 0
-        self.merged_cloud = get_default_message()
+        self.cloud_list = []
 
     def registered_callback(self, data):
-        # print('ORIGINAL:')
-        # print('header', data.header, 'height', data.height, 'width', data.width, 'fields', data.fields,
-        #       'bigend', data.is_bigendian, 'ptstep', data.point_step, 'rowstep', data.row_step, 'dense', data.is_dense)
-        self.merged_cloud.data += data.data
-        self.merged_cloud.width += data.width
+        if len(self.cloud_list) < AGGREGATION_COUNT:
+            self.cloud_list.append(data)
+            return
+        self.cloud_list.pop(0)
+        self.cloud_list.append(data)
 
-        self.frame_counter += 1
+        merged_cloud = get_default_message()
+        for cloud_msg in self.cloud_list:
+            merged_cloud.data += cloud_msg.data
+            merged_cloud.width += cloud_msg.width
 
-        if self.frame_counter >= AGGREGATION_COUNT:
-            self.merged_cloud.header = Header(stamp=rospy.Time.now(), frame_id='camera_init')
-            self.merged_cloud.row_step = self.merged_cloud.width * self.merged_cloud.point_step
+        merged_cloud.header = Header(stamp=self.cloud_list[-1].header.stamp, frame_id='camera_init')
+        merged_cloud.row_step = merged_cloud.width * merged_cloud.point_step
 
-            # print('NEW:')
-            # print('header', self.merged_cloud.header, 'height', self.merged_cloud.height, 'width', self.merged_cloud.width, 'fields', self.merged_cloud.fields,
-            #       'bigend', self.merged_cloud.is_bigendian, 'ptstep', self.merged_cloud.point_step, 'rowstep', self.merged_cloud.row_step, 'dense', self.merged_cloud.is_dense)
-            self.pub_aggregated.publish(self.merged_cloud)
-
-            self.frame_counter = 0
-            self.merged_cloud = get_default_message()
+        self.pub_aggregated.publish(merged_cloud)
 
 
 if __name__ == "__main__":
